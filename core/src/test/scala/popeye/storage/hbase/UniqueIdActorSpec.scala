fb7923c66f0bf49b23c14e44cb1bfc86f7047a8d
VasilyKolpakov <vasilykolpakov@gmail.com>
2014-07-08 13:54:23 +0400

    UniqueIdActor batching test

--- core/src/test/scala/popeye/storage/hbase/UniqueIdActorSpec.scala
+++ core/src/test/scala/popeye/storage/hbase/UniqueIdActorSpec.scala
@@ -8,13 +8,14 @@ import akka.actor.Props
 import popeye.storage.hbase.UniqueIdProtocol.{Resolved, FindName}
 import popeye.storage.hbase.HBaseStorage.{ResolvedName, QualifiedName}
 import akka.pattern.ask
-import scala.concurrent.Await
+import scala.concurrent.{Future, Await}
 import scala.concurrent.duration._
 import akka.util.Timeout
 
 
 class UniqueIdActorSpec extends AkkaTestKitSpec("uniqueid") with MockitoStubs {
-  implicit val timeout = Timeout(5 seconds)
+  implicit val timeout = Timeout(1 seconds)
+  implicit val executionContext = system.dispatcher
   behavior of "UniqueIdActor"
 
   it should "create new unique id" in {
@@ -31,5 +32,23 @@ class UniqueIdActorSpec extends AkkaTestKitSpec("uniqueid") with MockitoStubs {
     verify(storage).registerName(qName)
     verifyNoMoreInteractions(storage)
     response should equal(Resolved(ResolvedName(qName, id)))
+  }
+
+  it should "resolve many at once" in {
+    val storage = mock[UniqueIdStorageTrait]
+    val actor = TestActorRef(Props.apply(new UniqueIdActor(storage)))
+    val generationId = new BytesKey(Array[Byte](0, 0))
+    val resolvedNames = Seq(
+      ResolvedName(QualifiedName("kind", generationId, "first"), new BytesKey(Array[Byte](0))),
+      ResolvedName(QualifiedName("kind", generationId, "second"), new BytesKey(Array[Byte](1)))
+    )
+    stub(storage.findByName(resolvedNames.map(_.toQualifiedName))).toReturn(resolvedNames)
+    val idsFuture = Future.traverse(resolvedNames) {
+      rName => actor ? FindName(rName.toQualifiedName, create = true)
+    }
+    val response = Await.result(idsFuture, 5 seconds)
+    verify(storage).findByName(resolvedNames.map(_.toQualifiedName))
+    verifyNoMoreInteractions(storage)
+    response should equal(resolvedNames.map(rName => Resolved(rName)))
   }
 }
